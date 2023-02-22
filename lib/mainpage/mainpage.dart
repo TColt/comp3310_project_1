fbc9ad8c6a1f8a93bcae623fb3e0108d4b723b55
Tyler Coltrain <tcoltra1@my.westga.edu>
2023-02-22 18:50:00 -0500

    home buttons

--- lib/mainpage/mainpage.dart
+++ lib/mainpage/mainpage.dart
@@ -27,6 +27,44 @@ class SecondScreen extends StatelessWidget {
             }),
           ],
         ),
-        body: Container());
+        body: Padding(
+          padding: const EdgeInsets.only(top: 60.0),
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 30.0),
+                child: Center(),
+              ),
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: FloatingActionButton.extended(
+                    onPressed: () {},
+                    label: const Text('Messaging'),
+                  )),
+              const Padding(
+                padding: EdgeInsets.only(top: 30.0),
+              ),
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: FloatingActionButton.extended(
+                    onPressed: () {},
+                    label: const Text('Non Emergency Help'),
+                  )),
+              const Padding(
+                padding: EdgeInsets.only(top: 30.0),
+              ),
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: FloatingActionButton.extended(
+                    backgroundColor: Colors.red,
+                    onPressed: () {},
+                    label: const Text('Emergency Help'),
+                  )),
+              const Padding(
+                padding: EdgeInsets.only(top: 30.0),
+              ),
+            ],
+          ),
+        ));
   }
 }
